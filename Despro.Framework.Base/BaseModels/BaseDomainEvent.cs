47f418f2d49e1930683e2581af447e80645f7a4c
Alireza Gholiei <alireza.gholie@gmail.com>
2026-08-31 11:52:03 +0330

    Refactor BaseDomainEvent; bump version to 2.1.5
    Refactored BaseDomainEvent by removing redundant properties (Id, CreateDate, CreateUserId, DeleteDate, DeleteUserId) and introducing EventCreateDate initialized to current UTC ticks. Updated Despro.Framework.Base.csproj version from 2.1.4 to 2.1.5.

--- Despro.Framework.Base/BaseModels/BaseDomainEvent.cs
+++ Despro.Framework.Base/BaseModels/BaseDomainEvent.cs
@@ -4,9 +4,5 @@ namespace Despro.Framework.Base.BaseModels;
 
 public abstract class BaseDomainEvent : INotification
 {
-    public long Id { get; set; }
-    public long? CreateDate { get; protected set; } = DateTime.UtcNow.Ticks;
-    public long? CreateUserId { get; protected set; }
-    public long? DeleteDate { get; protected set; }
-    public long? DeleteUserId { get; protected set; }
+    public long? EventCreateDate { get; protected set; } = DateTime.UtcNow.Ticks;
 }
